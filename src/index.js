f695d97bf015b4c7b1e2e75ce6a16871e77d245e
akbarsenawjy26 <120768034+akbarsenawjy26@users.noreply.github.com>
2024-12-07 11:20:41 +0700

    feat(index): add new route & implement morgan to logging http request

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const db = require("../config/auth_database_config");
 require("dotenv").config();
 const app = express();
 const sessionStore = SequelizeStore(session.Store);
+const morgan = require("morgan");
+const fs = require("fs");
+const path = require("path");
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
+// const projectModel = require("./models/project_models");
 // const apikeyModel = require("./models/api-key_models");
 // const userModel = require("./models/user_models");
 // const userModel = require("./models/device_models");
@@ -19,6 +24,7 @@ const authRoutes = require("./auth/routes/auth_routes");
 const deviceRoutes = require("./device/routes/device_routes");
 const apiKeyRoutes = require("./api-key/routes/api-key_routes");
 const dashboardRoutes = require("./dashboard/routes/dashboard_routes");
+const projectRoutes = require("./project/routes/project_routes");
 
 // (async () => {
 //   await db.sync({ alter: true });
@@ -45,13 +51,15 @@ app.use(
 );
 
 app.use(express.json());
+app.use(morgan("combined", { stream: accessLogStream }));
 
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/device", deviceRoutes);
 app.use("/api/v1/api-key", apiKeyRoutes);
 app.use("/api/v1/dashboard", dashboardRoutes);
+app.use("/api/v1/project", projectRoutes);
 
 app.listen(3001, "0.0.0.0", () => {
-  console.log("Authentication App Running");
+  console.log("Middleware App Running");
 });
