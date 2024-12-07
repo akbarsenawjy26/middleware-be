1f3a98593c3c4e973715a2defa22b0143254b312
akbarsenawjy26 <120768034+akbarsenawjy26@users.noreply.github.com>
2024-12-07 11:15:57 +0700

    feat(db): changing terminal logging query state to false

--- config/auth_database_config.js
+++ config/auth_database_config.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 const sequelizeConnection = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT,
+  logging: false,
 });
 
 module.exports = sequelizeConnection;
